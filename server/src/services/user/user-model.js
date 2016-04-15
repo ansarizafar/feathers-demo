53a8fbf378df1ab3d6ef8efcc157d47b42107b33
zafar <zafar.ansari@live.com>
2016-04-16 00:16:03 +0500

    User and Customer model updated

--- server/src/services/user/user-model.js
+++ server/src/services/user/user-model.js
@@ -10,12 +10,12 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
 
-  name: { type: String, required: true },
+  userName: { type: String, required: true },
   role: { type: String, required: true, default: 'Customer' },
-  belongTo: { type: String },
+  belongTo: { type: Schema.Types.ObjectId },
   isActive: { type: Boolean, required: true, default: true },
 
-  email: {type: String, required: true, unique: true},
+  loginName: {type: String, required: true, unique: true},
   password: { type: String, required: true },
 
   createdAt: { type: Date, 'default': Date.now },
